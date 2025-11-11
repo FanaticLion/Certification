fd16fb24b4830a47021a4df84f955505785d1251
fantasticlion <den20020805@gmail.com>
2025-11-11 21:01:43 +0300

    feat: улучшение стилей админки Django
    - Исправлены выпадающие списки (видимость текста)
    - Увеличены ManyToMany фильтры
    - Улучшена адаптивность
    - Обновлена цветовая схема

--- config/urls.py
+++ config/urls.py
@@ -2,8 +2,43 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import render
+from network.models.network import NetworkNode, Product
+
+
+def home_view(request):
+    products = Product.objects.all()
+    network_nodes = NetworkNode.objects.all().select_related('contact', 'supplier')
+
+    factories = [node for node in network_nodes if node.node_type == 'factory']
+    retail_chains = [node for node in network_nodes if node.node_type == 'retail']
+    entrepreneurs = [node for node in network_nodes if node.node_type == 'entrepreneur']
+
+    context = {
+        'products': products,
+        'factories': factories,
+        'retail_chains': retail_chains,
+        'entrepreneurs': entrepreneurs,
+        'total_products': products.count(),
+        'total_nodes': network_nodes.count(),
+    }
+    return render(request, 'home.html', context)
+
+
+def product_catalog(request):
+    products = Product.objects.all()
+    return render(request, 'catalog.html', {'products': products})
+
+
+def stores_map(request):
+    nodes = NetworkNode.objects.all().select_related('contact')
+    return render(request, 'stores.html', {'nodes': nodes})
+
 
 urlpatterns = [
+    path('', home_view, name='home'),
+    path('catalog/', product_catalog, name='catalog'),
+    path('stores/', stores_map, name='stores'),
     path('admin/', admin.site.urls),
     path('api/', include('network.urls')),
     path('api/auth/', include('rest_framework.urls')),
--- users/admin.py
+++ users/admin.py
@@ -2,12 +2,18 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
+
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     list_display = ['username', 'email', 'first_name', 'last_name', 'is_active', 'is_staff']
     list_filter = ['is_active', 'is_staff', 'is_superuser']
-    fieldsets = UserAdmin.fieldsets + (
-        ('Дополнительные настройки', {
-            'fields': ('is_active',)
+
+    # Переопределяем fieldsets чтобы избежать дублирования
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Permissions', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
         }),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
     )
